d531f4a9b6512520d67beae2094fd0781c519540
Baset Hamza <75188299+BasetHamza@users.noreply.github.com>
2022-06-19 03:21:04 -0400

    restructuing research and BeeStream dropdown menus

--- appmais/src/components/App.js
+++ appmais/src/components/App.js
@@ -4,11 +4,11 @@ import { BrowserRouter as Navigate, Route, Router, Routes } from 'react-router-d
 import NavigationBar from './NavigationBar'
 import Footer from './Footer/Footer'
 import HomePage from '../pages/HomePage'
-import BeeStream from "../pages/BeeStream"
+import BeeStream from "../pages/BeeStream/LiveStream"
 import BeeLive from '../pages/BeeLive'
 
 class App extends Component {
-    
+
     render () {
         return (
             <div>
