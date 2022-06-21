890e4da8b577faff8fe4a67b57113d24b70534a7
Baset Hamza <75188299+BasetHamza@users.noreply.github.com>
2022-06-21 15:41:43 -0400

    editing homepage for deployment

--- appmais/src/index.js
+++ appmais/src/index.js
@@ -8,7 +8,7 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <BrowserRouter basename="/AppMAIS-Webpage">
+  <BrowserRouter basename="/AppMAIS-Webpage/">
         <App />
   </BrowserRouter>,
   document.getElementById('root')
