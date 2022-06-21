3fe6dc961ff53a449719f198fa9b264d4b5f32b2
Baset Hamza <75188299+BasetHamza@users.noreply.github.com>
2022-06-21 17:54:48 -0400

    Thingsboard is blocking the dahsboard from the iframe, had to use a button

--- appmais/src/pages/Dashboard.js
+++ appmais/src/pages/Dashboard.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
-import IframeResizer from 'iframe-resizer-react'
+// import IframeResizer from 'iframe-resizer-react'
+
+import { Row, Container, Button} from 'react-bootstrap'
+
 
 class Dashboard extends Component {
 
@@ -29,12 +32,34 @@ class Dashboard extends Component {
             //     + " src='http://appmais.cs.appstate.edu:8081/dashboard/56b56840-e108-11ec-a4b2-f50e8dc46689?publicId=de49f250-ea30-11eb-a587-63f1ebdeaad7'/>"
             // }} />
 
-            <IframeResizer
-                log
-                heightCalculationMethod="bodyScroll"
-                src="http://appmais.cs.appstate.edu:8081/dashboard/56b56840-e108-11ec-a4b2-f50e8dc46689?publicId=de49f250-ea30-11eb-a587-63f1ebdeaad7"
-                style={{ width: '1px', minWidth: '100%', height: "650px"}}
-            />
+            // <IframeResizer
+            //     log
+            //     heightCalculationMethod="bodyScroll"
+            //     src="http://appmais.cs.appstate.edu:8081/dashboard/56b56840-e108-11ec-a4b2-f50e8dc46689?publicId=de49f250-ea30-11eb-a587-63f1ebdeaad7"
+            //     style={{ width: '1px', minWidth: '100%', height: "650px"}}
+            // />
+
+            <div className="homepage blank-page">
+                <Container className='text-light'>
+                    <Row>
+                        <h1 class="display-1">AppMAIS</h1>
+                    </Row>
+                    <Row className='text-center mt-5'>
+                        <h3 class="display-3">Click the following link to view the dashboard</h3>
+                    </Row>
+                    <Row className="mt-5">
+                        <div className=" d-flex justify-content-center">
+                        <Button variant="dark" size="lg" active>
+                            <a href='http://appmais.cs.appstate.edu:8081/dashboard/56b56840-e108-11ec-a4b2-f50e8dc46689?publicId=de49f250-ea30-11eb-a587-63f1ebdeaad7'
+                            target='_blank'
+                            rel="noreferrer">
+                                View Live Dashboard</a>
+                        </Button>{' '}
+                        </div>
+                    </Row> 
+                </Container>
+            </div>
+
         )
     }
 }
