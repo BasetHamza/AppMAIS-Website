caad2afb98062c9ecc65d2a47ae30d7092d420a3
Baset Hamza <75188299+BasetHamza@users.noreply.github.com>
2022-06-21 11:29:52 -0400

    stream and archive are directed, and research overview is under \construction

---
+++ appmais/src/pages/Research/Overview.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+import { Card, Row, Col, Button, ImgOverlay, Container, Nav, Figure, Image} from 'react-bootstrap'
+
+class Overview extends Component{
+
+    render(){
+        return(
+                <div className="homepage blank-page">
+                <Container className='text-light'>
+                    <Row>
+                        <h1 class="display-1">AppMAIS</h1>
+                    </Row>
+                    <Row className='text-center mt-5'>
+                        <h3 class="display-3">Page IS UNDER CONSTRUCTION</h3>
+                    </Row>
+                    <Row>
+                        <h4 class="display-4 text-center mt-5">BEE PATIENT</h4>
+                    </Row> 
+                </Container>
+
+            </div>
+        )
+    }
+}
+
+
+export default Overview
